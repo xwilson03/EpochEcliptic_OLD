e173447a007d1f3b6f3af0b39daff9e416486143
Xander Wilson <xanderwilson2014@gmail.com>
2024-04-22 15:14:35 -0500

    fixes ObjectPool.spawn logic and makes void

--- EpochEcliptic/Assets/Scripts/Util/ObjectPool.cs
+++ EpochEcliptic/Assets/Scripts/Util/ObjectPool.cs
@@ -13,19 +13,20 @@ public class ObjectPool
         ExpandPool(startCapacity);
     }
 
-    public Poolable Spawn<U>(U spawnData) {
+    public void Spawn<U>(U spawnData) {
         Poolable obj = null;
 
         int i;
         for (i = 0; i < pool.Count; i++) {
-            obj = pool[i];
-            if (!obj.gameObject.activeInHierarchy) break;
+            if (!pool[i].gameObject.activeInHierarchy) {
+                obj = pool[i];
+                obj.gameObject.SetActive(true);
+                break;
+            }
         }
 
         if (obj == null) obj = ExpandPool();
         obj.Init(i, spawnData);
-        
-        return obj;
     }
 
     public void Kill(object sender, int id) {
@@ -36,6 +37,7 @@ public class ObjectPool
         int size = pool.Count;
         for (int i = 0; i < count; i++) {
             Poolable obj = Object.Instantiate(prefab).GetComponent<Poolable>();
+
             obj.OnDeath += Kill;
             pool.Add(obj);
         }
