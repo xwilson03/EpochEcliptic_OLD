a9e649b9bd0dbd5053ffebdd2d2a16bed531143d
Xander Wilson <xanderwilson2014@gmail.com>
2024-04-26 03:41:49 -0500

    refactors options menu script

--- EpochEcliptic/Assets/Scripts/UI/Menus/OptionsMenu.cs
+++ EpochEcliptic/Assets/Scripts/UI/Menus/OptionsMenu.cs
@@ -5,12 +5,14 @@ using UnityEngine.UI;
 public class OptionsMenu : Menu {
     
     [SerializeField] Button accessibilityButton;
-    [SerializeField] Button lessMotionButton;
-    [SerializeField] Button backButton;
-
     [SerializeField] TextMeshProUGUI accessibilityText;
+
+    [SerializeField] Button lessMotionButton;
     [SerializeField] TextMeshProUGUI lessMotionText;
+    
     [SerializeField] Slider volumeSlider;
+
+    [SerializeField] Button backButton;
 
     [SerializeField] Texture2D cursorTexture;
 
@@ -18,10 +20,10 @@ public class OptionsMenu : Menu {
         base.Awake();
         
         Util.CheckReference(name, "Accessibility Button", accessibilityButton);
-        Util.CheckReference(name, "Less Motion Button", lessMotionButton);
-        Util.CheckReference(name, "Back Button", backButton);
         Util.CheckReference(name, "Accessibility Text", accessibilityText);
+        Util.CheckReference(name, "Less Motion Button", lessMotionButton);
         Util.CheckReference(name, "Less Motion Text", lessMotionText);
+        Util.CheckReference(name, "Back Button", backButton);
         Util.CheckReference(name, "Volume Slider", volumeSlider);
 
         Util.CheckReference(name, "Cursor Texture", cursorTexture);
@@ -30,35 +32,29 @@ public class OptionsMenu : Menu {
         lessMotionButton.onClick.AddListener(delegate    {ToggleLessMotion();});
         backButton.onClick.AddListener(delegate          {MenuController.Back();});
 
-        accessibilityText.text =
-            "Accessibility:     " + (PlayerPrefs.GetInt("accessibility") == 1 ? "Enabled" : "Disabled");
-        lessMotionText.text =
-            "Less Motion:        " + (PlayerPrefs.GetInt("lessMotion") == 1 ? "Enabled" : "Disabled");
+        accessibilityText.text = Util.BoolToStatus(Util.GetBool("accessibility", true));
+        lessMotionText.text =    Util.BoolToStatus(Util.GetBool("lessMotion", false));
 
-        volumeSlider.normalizedValue = PlayerPrefs.HasKey("volume") ? PlayerPrefs.GetFloat("volume") : 1f;
+        volumeSlider.normalizedValue = Util.GetFloat("volume", 1f);
         AudioListener.volume = volumeSlider.normalizedValue;
 
         Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.Auto);
     }
 
     public void SetVolume() {
-        PlayerPrefs.SetFloat("volume", volumeSlider.normalizedValue);
+        Util.SetFloat("volume", volumeSlider.normalizedValue);
         AudioListener.volume = volumeSlider.normalizedValue;
     }
 
     public void ToggleLessMotion() {
-        PlayerPrefs.SetInt("lessMotion",
-            PlayerPrefs.GetInt("lessMotion") == 1 ? 0 : 1);
-        lessMotionText.text = "Less Motion:        "
-            + (PlayerPrefs.GetInt("lessMotion") == 1 ? "Enabled" : "Disabled");
+        Util.ToggleBool("lessMotion");
+        lessMotionText.text = Util.BoolToStatus(Util.GetBool("lessMotion"));
     }
 
     public void ToggleAccessibility() {
-        PlayerPrefs.SetInt("accessibility",
-            PlayerPrefs.GetInt("accessibility") == 1 ? 0 : 1);
-        accessibilityText.text =
-            "Accessibility:     " + (PlayerPrefs.GetInt("accessibility") == 1 ? "Enabled" : "Disabled");
+        Util.ToggleBool("accessibility");
+        accessibilityText.text = Util.BoolToStatus(Util.GetBool("accessibility"));
 
-        UAP_AccessibilityManager.EnableAccessibility(PlayerPrefs.GetInt("accessibility") == 1);
+        UAP_AccessibilityManager.EnableAccessibility(Util.GetBool("accessibility"));
     }
 }
