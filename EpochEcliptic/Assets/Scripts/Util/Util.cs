ad1491677ea77928d9e50748b610fdbbbfd1f497
Xander Wilson <xanderwilson2014@gmail.com>
2024-04-26 03:41:30 -0500

    adds pref helpers to Util

--- EpochEcliptic/Assets/Scripts/Util/Util.cs
+++ EpochEcliptic/Assets/Scripts/Util/Util.cs
@@ -40,4 +40,51 @@ public class Util {
 
         return true;
     }
+
+    public static int GetInt(string pref) {
+        return PlayerPrefs.GetInt(pref);
+    }
+
+    public static int GetInt(string pref, int defaultVal) {
+        if (!PlayerPrefs.HasKey(pref)) PlayerPrefs.SetInt(pref, defaultVal);
+        return PlayerPrefs.GetInt(pref);
+    }
+
+    public static void SetInt(string pref, int val) {
+        PlayerPrefs.SetInt(pref, val);
+    }
+
+    public static float GetFloat(string pref) {
+        return PlayerPrefs.GetFloat(pref);
+    }
+
+    public static float GetFloat(string pref, float defaultVal) {
+        if (!PlayerPrefs.HasKey(pref)) PlayerPrefs.SetFloat(pref, defaultVal);
+        return PlayerPrefs.GetFloat(pref);
+    }
+
+    public static void SetFloat(string pref, float val) {
+        PlayerPrefs.SetFloat(pref, val);
+    }
+
+    public static bool GetBool(string pref) {
+        return PlayerPrefs.GetInt(pref) != 0;
+    }
+
+    public static bool GetBool(string pref, bool defaultVal) {
+        if (!PlayerPrefs.HasKey(pref)) PlayerPrefs.SetInt(pref, defaultVal ? 1 : 0);
+        return PlayerPrefs.GetInt(pref) != 0;
+    }
+
+    public static void SetBool(string pref, bool val) {
+        PlayerPrefs.SetInt(pref, val ? 1 : 0);
+    }
+
+    public static void ToggleBool(string pref) {
+        SetBool(pref, !GetBool(pref));
+    }
+
+    public static string BoolToStatus(bool val) {
+        return val ? "Enabled" : "Disabled";
+    }
 }
