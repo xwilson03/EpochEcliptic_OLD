4f90c9a18df5b0a28b0414a98a9e2ff8068cd186
xwilson03 <xanderwilson2014@gmail.com>
2024-05-04 02:52:42 -0500

    prevents game input while in menu

--- EpochEcliptic/Assets/Scripts/InputController.cs
+++ EpochEcliptic/Assets/Scripts/InputController.cs
@@ -13,33 +13,35 @@ public class InputController : MonoBehaviour {
             else MenuController.Exit();
         }
 
-        // Get desired direction from keyboard input
-        Vector2 desired = Vector2.zero;
+        if (!MenuController.InMenu()) {
+            // Get desired direction from keyboard input
+            Vector2 desired = Vector2.zero;
 
-        if (Input.GetKey(KeyCode.W)) desired.y += 1;
-        if (Input.GetKey(KeyCode.S)) desired.y -= 1;
-        if (Input.GetKey(KeyCode.A)) desired.x -= 1;
-        if (Input.GetKey(KeyCode.D)) desired.x += 1;
+            if (Input.GetKey(KeyCode.W)) desired.y += 1;
+            if (Input.GetKey(KeyCode.S)) desired.y -= 1;
+            if (Input.GetKey(KeyCode.A)) desired.x -= 1;
+            if (Input.GetKey(KeyCode.D)) desired.x += 1;
 
-        // Move player (function normalizes vector)
-        if (desired != Vector2.zero) {
-            Refs.player.Move(desired);
-        }
+            // Move player (function normalizes vector)
+            if (desired != Vector2.zero) {
+                Refs.player.Move(desired);
+            }
 
-        // Fire if left mouse button is held
-        if (Input.GetMouseButton(0)) {
-            
-            // Get mouse location in game world (adjust Z value to prevent view clipping)
-            Vector3 realMousePos = Input.mousePosition;
-            realMousePos.z = -Camera.main.transform.position.z;
-            realMousePos = Camera.main.ScreenToWorldPoint(realMousePos);
-            
-            Refs.player.Shoot(realMousePos);
-        }
+            // Fire if left mouse button is held
+            if (Input.GetMouseButton(0)) {
+                
+                // Get mouse location in game world (adjust Z value to prevent view clipping)
+                Vector3 realMousePos = Input.mousePosition;
+                realMousePos.z = -Camera.main.transform.position.z;
+                realMousePos = Camera.main.ScreenToWorldPoint(realMousePos);
+                
+                Refs.player.Shoot(realMousePos);
+            }
 
-        // Activate ability if right mouse button or space held
-        if (Input.GetMouseButton(1) || Input.GetKey(KeyCode.Space)) {
-            Refs.player.ActivateAbility();
+            // Activate ability if right mouse button or space held
+            if (Input.GetMouseButton(1) || Input.GetKey(KeyCode.Space)) {
+                Refs.player.ActivateAbility();
+            }
         }
     }
 }
