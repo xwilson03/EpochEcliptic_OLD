34172bee057c3100d81bb746e7c7fdb642af1a28
xwilson03 <xanderwilson2014@gmail.com>
2024-05-05 19:10:30 -0500

    adds controls link to pause menu

--- EpochEcliptic/Assets/Scripts/UI/Menus/PauseMenu.cs
+++ EpochEcliptic/Assets/Scripts/UI/Menus/PauseMenu.cs
@@ -5,6 +5,7 @@ public class PauseMenu : Menu {
     
     [SerializeField] Button resumeButton;
     [SerializeField] Button optionsButton;
+    [SerializeField] Button controlsButton;
     [SerializeField] Button quitButton;
 
     new void Awake() {
@@ -12,10 +13,12 @@ public class PauseMenu : Menu {
 
         Util.CheckReference(name, "Resume Button", resumeButton);
         Util.CheckReference(name, "Options Button", optionsButton);
+        Util.CheckReference(name, "Controls Button", controlsButton);
         Util.CheckReference(name, "Quit Button", quitButton);
 
         resumeButton.onClick.AddListener(delegate  {MenuController.Exit();});
         optionsButton.onClick.AddListener(delegate {MenuController.GoTo("Options");});
+        controlsButton.onClick.AddListener(delegate {MenuController.GoTo("Controls");});
         quitButton.onClick.AddListener(delegate    {Refs.fader.FadeTo("MainMenu");});
     }
 }
